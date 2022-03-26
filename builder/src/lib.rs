0ae484dc45d2f3779e760d954b722f7a2ad0c31c
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-03-27 07:26:23 +0800

    Redo Builder, nicely done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,301 +1,200 @@
-use proc_macro2::Span;
+use proc_macro2::{Ident, Literal, Span, TokenTree};
+use quote::quote;
+use syn::spanned::Spanned;
 use syn::{
-    braced,
-    parse::{Parse, Result},
-    spanned::Spanned,
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, GenericArgument, PathArguments,
+    PathSegment, Type,
 };
-use syn::{punctuated::Punctuated, Visibility};
-use syn::{Ident, Token};
+use syn::{Error, Result};
 
-#[derive(Debug)]
-struct BuildError(String);
+type TokenStream1 = proc_macro::TokenStream;
+type TokenStream2 = proc_macro2::TokenStream;
 
-impl std::error::Error for BuildError {}
-
-impl std::fmt::Display for BuildError {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(formatter, "{}", self.0)
+fn data_struct(data: Data) -> Result<DataStruct> {
+    match data {
+        Data::Struct(d) => Ok(d),
+        Data::Enum(e) => Err(Error::new(e.enum_token.span, "Expect Data::Struct")),
+        Data::Union(e) => Err(Error::new(e.union_token.span, "Expect Data::Struct")),
     }
 }
 
-#[derive(Debug, Default)]
-struct StructDef {
-    vis: Option<Visibility>,
-    origin: Option<syn::Ident>,
-    extended: Option<syn::Ident>,
-    members: Option<Vec<Member>>,
-}
-
-#[derive(Debug, Default)]
-struct Member {
-    attr: Option<Attribute>,
-    ident: Option<Ident>,
-    ty: Option<syn::Type>,
-}
-
-#[derive(Debug)]
-struct Attribute {
-    path: Option<syn::Ident>,
-    lhs_tok: syn::Ident,
-    punct: Token![=],
-    rhs_tok: syn::LitStr,
-}
-
-impl Parse for Attribute {
-    fn parse(input: syn::parse::ParseStream) -> Result<Self> {
-        let lhs: syn::Ident = input.parse()?;
-        let span = lhs.span();
-        let p: Token![=] = input.parse()?;
-        let rhs: syn::LitStr = input.parse()?;
-        if lhs.to_string() != "each" {
-            return Err(syn::Error::new(span, r#"expected `builder(each = "...")`"#));
+fn path_segment_from_type(ty: &Type) -> Result<&PathSegment> {
+    if let Type::Path(tp) = ty {
+        let segs = &tp.path.segments;
+        if segs.len() == 1 {
+            return Ok(segs.first().unwrap());
         }
-        Ok(Attribute {
-            path: None,
-            lhs_tok: lhs,
-            punct: p,
-            rhs_tok: rhs,
-        })
     }
+    Err(Error::new(ty.span(), "Fail to get PathSegment"))
 }
 
-impl Parse for StructDef {
-    fn parse(input: syn::parse::ParseStream) -> Result<Self> {
-        let vis: Visibility = input.parse()?;
-        let _: Token![struct] = input.parse()?;
-        let name: Ident = input.parse()?;
-        let content;
-        let _: syn::token::Brace = braced!(content in input);
-        let fields: Punctuated<syn::Field, Token![,]> =
-            content.parse_terminated(syn::Field::parse_named)?;
+fn is_option(ty: &Type) -> (bool, Option<&Ident>) {
+    wrapped_type(ty, "Option")
+}
 
-        let mut members = Vec::new();
-        for field in fields.iter() {
-            let mut member: Member = Default::default();
-            member.ident = field.ident.clone();
-            for attr in field.attrs.iter() {
-                let mut attr2: Attribute;
-                let path = Some(attr.path.get_ident().unwrap().clone());
-                match attr.parse_args() {
-                    Ok(attr) => {
-                        attr2 = attr;
-                    }
-                    Err(e) => {
-                        let mut span = attr.path.span();
-                        span = span.join(attr.tokens.span()).unwrap_or(attr.tokens.span());
-                        return Err(syn::Error::new(span, e));
+fn wrapped_type<'a>(ty: &'a Type, wrapper: &'a str) -> (bool, Option<&'a Ident>) {
+    if let Ok(sg) = path_segment_from_type(ty) {
+        if sg.ident == wrapper {
+            if let PathArguments::AngleBracketed(args) = &sg.arguments {
+                let args = &args.args;
+                if args.len() == 1 {
+                    if let Some(GenericArgument::Type(ty)) = args.first() {
+                        if let Ok(sg) = path_segment_from_type(ty) {
+                            return (true, Some(&sg.ident));
+                        }
                     }
                 }
-                attr2.path = path;
-                member.attr = Some(attr2);
             }
-            member.ty = Some(field.ty.clone());
-            members.push(member);
+        }
+    }
+    (false, None)
+}
+
+fn builder_attribute(field: &Field) -> Result<(bool, Option<Literal>)> {
+    let attrs = &field.attrs;
+    if attrs.len() == 1 {
+        let attr = attrs.first().unwrap();
+        let segs = &attr.path.segments;
+        if segs.len() == 1 && segs[0].ident == "builder" {
+            let mut tokens = attr.tokens.clone().into_iter();
+            let token = tokens.next().unwrap();
+            if let TokenTree::Group(grp) = token {
+                let mut tokens = grp.stream().into_iter();
+                let ident = tokens.next().unwrap();
+                let punct = tokens.next().unwrap();
+                if ident.to_string() == "each" {
+                    if punct.to_string() == "=" {
+                        let literal = tokens.next().unwrap();
+                        if let TokenTree::Literal(literal) = literal {
+                            return Ok((true, Some(literal)));
+                        }
+                    }
+                } else {
+                    return ::std::result::Result::Err(syn::Error::new(
+                        grp.span(),
+                        "expected `builder(each = \"...\")`".to_string(),
+                    ));
+                }
+            }
+        }
+    }
+    Ok((false, None))
+}
+
+fn builder_deriver(input: DeriveInput) -> Result<TokenStream2> {
+    let vis = input.vis;
+    let head = input.ident;
+    let _generics = input.generics;
+    let data = input.data;
+    let builder = Ident::new(
+        &(String::from(&head.to_string()) + "Builder"),
+        Span::call_site(),
+    );
+    let data = data_struct(data).unwrap();
+
+    let mut fields_declarer = quote! {};
+    let mut fields_initializer = quote! {};
+    let mut fields_setter = quote! {};
+    let mut fields_copy = quote! {};
+    for field in &data.fields {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+        let (is_option, wrapped_id) = is_option(ty);
+
+        let mut alt_decl = quote! { ::std::option::Option<#ty> };
+        let mut alt_sett = quote! { #ty };
+        let s = format!("{}: Field is None ", &ident.to_string());
+        let mut alt_copy = quote! { .expect(#s)  };
+
+        let (is_repeated, each) = builder_attribute(field)?;
+
+        if is_option {
+            alt_decl = quote! { #ty };
+            alt_sett = quote! { #wrapped_id };
+            alt_copy = quote! {};
         }
 
-        Ok(StructDef {
-            vis: Some(vis),
-            origin: Some(name.clone()),
-            extended: Some(syn::Ident::new(
-                &format!("{}Builder", name),
+        fields_declarer = quote! {
+            #fields_declarer
+            #ident: #alt_decl,
+        };
+
+        if is_repeated {
+            let (_, inner) = wrapped_type(ty, "Vec");
+            let e = each.unwrap();
+            let each = Ident::new(
+                &e.to_string()[1..e.to_string().len() - 1],
                 Span::call_site(),
-            )),
-            members: Some(members),
-        })
-    }
-}
-
-#[derive(Debug)]
-struct Element {
-    builder_setter: proc_macro2::TokenStream,
-    builder_default: proc_macro2::TokenStream,
-    builder_declare: proc_macro2::TokenStream,
-    builder_to_base_assign: proc_macro2::TokenStream,
-}
-
-#[proc_macro_derive(Builder, attributes(builder))]
-pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let def = syn::parse_macro_input!(input as StructDef);
-    let vis = def.vis.unwrap();
-    let base = def.origin.unwrap();
-    let builder = def.extended.unwrap();
-
-    let mut v = Vec::new();
-    for m in def.members.as_ref().unwrap().iter() {
-        let extmem;
-        let base = m.ident.as_ref().unwrap();
-        let builder_declare;
-        let builder_to_base_assign;
-        let builder_setter;
-        let mut core_ty = m.ty.as_ref().unwrap().clone();
-        let has_attr = m.attr.is_some();
-
-        if has_attr {
-            if let Ok(ty) = core_type_of_vec(m.ty.as_ref().unwrap()) {
-                core_ty = ty;
-            }
-        }
-
-        if let Ok(ty) = core_type_of_option(m.ty.as_ref().unwrap()) {
-            core_ty = ty;
-        }
-
-        if let Some(ref attr) = m.attr {
-            extmem = syn::Ident::new(&attr.rhs_tok.value(), Span::call_site());
-            builder_setter = quote::quote_spanned! {extmem.span()=>
-                fn #extmem(&mut self, arg: #core_ty) -> &mut Self {
-                    self.#extmem.as_mut().unwrap().push(arg);
+            );
+            let inner = inner.unwrap();
+            fields_setter = quote! {
+                #fields_setter
+                fn #each(&mut self, #each: #inner) -> &mut Self {
+                    if self.#ident.is_none() {
+                        self.#ident = ::std::option::Option::Some(::std::vec::Vec::new());
+                    }
+                    self.#ident.as_mut().unwrap().push(#each);
                     self
                 }
             };
+            fields_copy = quote! {
+                #fields_copy
+                #ident: match self.#ident {
+                    None => ::std::vec::Vec::new(),
+                    _ => self.#ident.clone()#alt_copy
+                },
+            };
         } else {
-            extmem = m.ident.as_ref().unwrap().clone();
-            builder_setter = quote::quote_spanned! {extmem.span()=>
-                #vis fn #extmem(&mut self, arg: #core_ty) -> &mut Self {
-                    self.#extmem = Some(arg);
+            fields_setter = quote! {
+                #fields_setter
+                fn #ident(&mut self, #ident: #alt_sett) -> &mut Self {
+                    self.#ident = ::std::option::Option::Some(#ident);
                     self
                 }
             };
-        }
 
-        let ty = &m.ty;
-        if is_option(m.ty.as_ref().unwrap()) {
-            builder_declare = quote::quote! {
-                #extmem: #ty,
-            };
-            builder_to_base_assign = quote::quote! {
-                #base: self.#extmem.clone(),
-            };
-        } else {
-            builder_declare = quote::quote! {
-                #extmem: ::std::option::Option<#ty>,
-            };
-            builder_to_base_assign = quote::quote! {
-                #base: self.#extmem.as_ref().unwrap().clone(),
-            };
-        }
-        let builder_default;
-        if is_vec(m.ty.as_ref().unwrap()) {
-            builder_default = quote::quote! {
-                #extmem: Some(::std::vec::Vec::new()),
-            };
-        } else {
-            builder_default = quote::quote! {
-                #extmem: None,
+            fields_copy = quote! {
+                #fields_copy
+                #ident: self.#ident.clone()#alt_copy,
             };
         }
 
-        v.push(Element {
-            builder_setter: builder_setter,
-            builder_default: builder_default,
-            builder_declare: builder_declare,
-            builder_to_base_assign: builder_to_base_assign,
-        });
+        fields_initializer = quote! {
+            #fields_initializer
+            #ident: None,
+        };
     }
-
-    let member_def = v.iter().map(|e| &e.builder_declare);
-    let define_builder_struct = quote::quote! {
-
-        #vis struct #builder {
-            #(#member_def)*
-        }
-    };
-
-    let builder_default = v.iter().map(|e| &e.builder_default);
-    let impl_base = quote::quote! {
-        impl #base {
-            #vis fn builder() -> #builder {
-                #builder {
-                    #(#builder_default)*
-                }
-            }
-        }
-    };
-
-    let builder_to_base_assign = v.iter().map(|e| &e.builder_to_base_assign);
-    let builder_builds = quote::quote! {
-        #vis fn build(&self) -> ::std::result::Result<#base, ::std::boxed::Box<dyn std::error::Error>> {
-            Ok(#base {
-                #(#builder_to_base_assign)*
+    let build_func = quote! {
+        pub fn build(&mut self) -> ::std::result::Result<#head, ::std::boxed::Box<dyn std::error::Error>> {
+            Ok(#head {
+                #fields_copy
             })
         }
     };
 
-    let builder_setter = v.iter().map(|e| &e.builder_setter);
-
-    let impl_builder = quote::quote! {
-        impl #builder {
-            #(#builder_setter)*
-            #builder_builds
+    Ok(quote! {
+        pub struct #builder {
+            #fields_declarer
         }
-    };
-
-    proc_macro::TokenStream::from(quote::quote! {
-        #define_builder_struct
-        #impl_builder
-        #impl_base
+        impl #head {
+            #vis fn builder() -> #builder
+            {
+                #builder {
+                    #fields_initializer
+                }
+            }
+        }
+        impl #builder {
+            #fields_setter
+            #build_func
+        }
     })
 }
 
-macro_rules! find_type {
-    ($src:expr,$target:expr) => {{
-        let mut res = false;
-        if let syn::Type::Path(ref path) = $src {
-            for segment in &path.path.segments {
-                if segment.ident.to_string() == stringify!($target) {
-                    res = true;
-                    break;
-                }
-            }
-        }
-        res
-    }};
-}
-
-fn is_option(ty: &syn::Type) -> bool {
-    find_type!(ty, Option)
-}
-
-fn is_vec(ty: &syn::Type) -> bool {
-    find_type!(ty, Vec)
-}
-
-fn core_type_of_vec(ty: &syn::Type) -> std::result::Result<syn::Type, BuildError> {
-    if let syn::Type::Path(ref path) = ty {
-        for segment in &path.path.segments {
-            if segment.ident.to_string() == "Vec" {
-                if let syn::PathArguments::AngleBracketed(ref args) = segment.arguments {
-                    if args.args.len() == 1 {
-                        if let syn::GenericArgument::Type(ref ty) = args.args.iter().next().unwrap()
-                        {
-                            return Ok(ty.clone());
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Err(BuildError(
-        "Don't know what is inside Vec, or Vec doesn't exist.".to_string(),
-    ))
-}
-
-fn core_type_of_option(ty: &syn::Type) -> std::result::Result<syn::Type, BuildError> {
-    if let syn::Type::Path(ref path) = ty {
-        for segment in &path.path.segments {
-            if segment.ident.to_string() == "Option" {
-                if let syn::PathArguments::AngleBracketed(ref args) = segment.arguments {
-                    if args.args.len() == 1 {
-                        if let syn::GenericArgument::Type(ref ty) = args.args.iter().next().unwrap()
-                        {
-                            return Ok(ty.clone());
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Err(BuildError(
-        "Don't know what is inside Vec, or Vec doesn't exist.".to_string(),
-    ))
+#[proc_macro_derive(Builder, attributes(builder))]
+pub fn derive(input: TokenStream1) -> TokenStream1 {
+    let input = parse_macro_input!(input as DeriveInput);
+    builder_deriver(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
