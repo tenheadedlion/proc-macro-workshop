f095bd069f5e18bb8e0670efb56fef829cd60479
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-03-29 03:39:51 +0800

    Merge Update

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,9 +1,27 @@
-use proc_macro::TokenStream;
+use proc_macro2::{Ident, Literal, Span, TokenTree};
+use quote::quote;
+use syn::spanned::Spanned;
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, GenericArgument, PathArguments,
+    PathSegment, Type,
+};
+use syn::{Error, Result};
+
+type TokenStream1 = proc_macro::TokenStream;
+type TokenStream2 = proc_macro2::TokenStream;
+
+
+
+
+
 
 #[proc_macro_attribute]
-pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn bitfield(args: TokenStream1, input: TokenStream1) -> TokenStream1 {
+
+    dbg!(args);
+    dbg!(input);
+    
+
 
     unimplemented!()
 }
